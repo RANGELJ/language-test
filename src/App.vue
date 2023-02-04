233ebfd8d9430b39ec15414a97537e503f0fa4d2
Jorge Rangel <jorgerangel@192.168.1.3>
2023-02-04 10:14:14 -0600

    Set up canvas to render editor

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
+  height: 100%;
 }
 
 nav {

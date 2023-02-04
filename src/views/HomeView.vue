6da407f60ef55b736e039690e0c681aef7618471
Jorge Rangel <jorgerangel@192.168.1.3>
2023-02-04 10:17:58 -0600

    Text area ready for code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>Hello</div>
+  <div class="editorContainer">
+    <textarea
+      class="codeEditor"
+      aria-label="Code editor"
+    />
+  </div>
 </template>
 
 <script lang="ts" setup>
 </script>
+
+<style scoped>
+.editorContainer {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 10fr 1fr 10fr 1fr;
+  grid-template-rows: 1fr 10fr 1fr;
+}
+
+.codeEditor {
+  grid-column: 2 / 3;
+  grid-row: 2 / 3;
+}
+</style>
